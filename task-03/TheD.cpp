9954aaae6185f3cc34e1e95c57fae02b1d52e2ef
niharsai02 <72076929+niharsai02@users.noreply.github.com>
2020-11-30 19:55:45 +0530

    Update TheD.cpp

--- task-03/TheD.cpp
+++ task-03/TheD.cpp
@@ -3,27 +3,42 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+
+
 using namespace std;
 
 
-
 int main (){
-int k;
-cin>>k;
-int n;
-n=(k+1)/2;
 
-for (int i=1;i<=n;i++){
+int a; // input
+    
 
-    for (int j=1;j<=n-i;j++){cout << '*';}
+cin>>a;
+
+int h;
+
+h=(a+1)/2; // dividing into half
+
+for (int i=1;i<=h;i++){ //for first half
+
+    for (int j=1;j<=h-i;j++){cout << '*';}
+    
     for (int j=1;j<=2*i-1;j++){cout << 'D';}
-    for (int j=1;j<=n-i;j++){cout << '*';}
+    
+    for (int j=1;j<=h-i;j++){cout << '*';}
 cout <<endl;}
-n=n-1;
-for(int i=n;i>=1;i--){
-for(int j=1;j<=n-i+1;j++){cout<<'*';}
+
+h=h-1;
+
+for(int i=h;i>=1;i--){   //for second half
+
+for(int j=1;j<=h-i+1;j++){cout<<'*';}
+
 for (int j=1;j<=2*i-1;j++){cout << 'D';}
-for(int j=1;j<=n-i+1;j++){cout<<'*';}
+
+for(int j=1;j<=h-i+1;j++){cout<<'*';}
+
+    
 cout<< endl;
 }
 return 0;}
